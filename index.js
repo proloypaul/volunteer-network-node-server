a09ff5adeb2c803e2b093393722aa449f54b54a9
proloypaul <proloyp23@gmail.com>
2021-10-28 01:03:46 +0600

    dotenv add

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
+require('dotenv').config()
 const app = express()
-const port = 4000;
+const port = process.env.PORT || 4000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
 app.get('/', (req, res) => {
     res.send("volunteer server open");
 });
