a679db0a597c156f06a014923d93b5132e9c3260
proloypaul <proloyp23@gmail.com>
2021-10-28 22:02:16 +0600

    add some method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
+const ObjectId = require('mongodb').ObjectId
 const port = process.env.PORT || 4000;
 
+app.use(express.json())
+app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // console.log(uri)
@@ -14,6 +18,7 @@ async function run() {
         // console.log("database working")
         const database = client.db("volunteerWork")
         const topicCollection = database.collection("topic")
+        const userCollection = database.collection("users")
 
         // GET method to show data in my server from database
         app.get('/topicdata', async (req, res) => {
@@ -23,6 +28,32 @@ async function run() {
             res.send(result)
         })
 
+        // POST method collect clint data send to database 
+        app.post('/users', async (req, res) => {
+            const userData = req.body
+            // console.log("user data", userData);
+            const result = await userCollection.insertOne(userData)
+            console.log("inset database result", result)
+            res.json(result)
+        })
+
+        // GET method to collect user data from database
+        app.get('/users', async (req, res) => {
+            const cursor = await userCollection.find({});
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // DELETE method to delete user
+        app.delete('/users:id', async (req, res) => {
+            const userId = req.params.id
+            console.log("user id ", userId)
+            const query = {_id: ObjectId(userId)}
+            const result = await userCollection.deleteOne(query);
+            console.log("delete result", result)
+            res.json(result)
+           
+        })
 
 
     }finally{
